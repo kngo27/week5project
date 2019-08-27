b678c5ff37b15f4b792546d6932ba4f4d4916969
Kien-Anh <kngo0001@student.monash.edu>
2019-08-27 13:40:47 +1000

    Update changes

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ let path2Views=__dirname+"/views/";
 let bodyParser=require('body-parser');
 app.engine("html", require("ejs").renderFile);
 app.set('view engine','html')
+app.use(express.static('views'))
 let db=[];
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.get('/',function(req,res){
-    res.sendFile(path2Views+"index.html");
+    res.render(path2Views+"index.html");
 });
 
 app.get('/addTask',function(req,res){
-    res.sendFile(path2Views+"newTask.html");
+    res.render(path2Views+"newTask.html");
 });
 
 
@@ -20,7 +21,7 @@ app.post('/newTaskRecord',function(req,res){
     console.log(req.body);
     db.push(req.body);
 
-    res.sendFile(path2Views+"index.html");
+    res.render(path2Views+"index.html");
 
     
 });
