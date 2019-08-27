7d39645dc73d40aaab19d50de12605c1e761caa4
Kien-Anh <kngo0001@student.monash.edu>
2019-08-27 13:08:05 +1000

    Initial Commit

--- app.js
+++ app.js
@@ -1,48 +1,34 @@
-var express = require('express');
-var app = express();
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-let bodyParser = require('body-parser');
+let express=require('express');
+let app=express();
+let path2Views=__dirname+"/views/";
+let bodyParser=require('body-parser');
+app.engine("html", require("ejs").renderFile);
+app.set('view engine','html')
+let db=[];
 
-let db = [];
+app.use(bodyParser.urlencoded({extended:false}));
+app.get('/',function(req,res){
+    res.sendFile(path2Views+"index.html");
+});
 
-
-
-// app.get('/',(req, res) => {
-//     res.render("showTasks.html")
-// })
-
-
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-// parse application/json
-app.use(bodyParser.json())
-app.get('/', function (req, res) {
-    res.render('showTasks.html')
+app.get('/addTask',function(req,res){
+    res.sendFile(path2Views+"newTask.html");
 });
 
 
-app.post('/data', function (req, res) {
-    console.log(req.body.taskId);
-    console.log(req.body.dueDate);
-    console.log(req.body.taskDesc);
-    res.send('Thank You')
+app.post('/newTaskRecord',function(req,res){
+    console.log(req.body);
+    db.push(req.body);
+
+    res.sendFile(path2Views+"index.html");
+
+    
+});
+
+app.get('/showAllTasks',(req,res)=>{
+    res.render(path2Views+"showTask.html",{
+        task:db
+    })
+
 })
-
-
-db.push({
-    carId: 3,
-    carMake: 'Audi',
-    carModel: 'A6',
-    carYear: 2019
-});
-
-app.get('/', function (req, res) {
-    res.render('showTasks.html', {carDb: db});
-});
-
 app.listen(8080);
-
